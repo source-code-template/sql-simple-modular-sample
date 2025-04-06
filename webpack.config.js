7e93f9fe89a35f5cf7b2cd08ad602a08cf9710f0
Duc Nguyen <minhduc140583@gmail.com>
2025-04-06 18:11:49 +0700

    Refactor code

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-const webpack = require('webpack'),
-  path = require('path'),
-  BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require("webpack"),
+  path = require("path"),
+  BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
 module.exports = {
-  target: 'node',
-  entry: './src/app.ts',
+  target: "node",
+  entry: "./src/app.ts",
   module: {
     rules: [
       {
@@ -12,19 +12,16 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "ts-loader",
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: [".ts", ".js"],
   },
   output: {
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  plugins: [
-    new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ }),
-    new BundleAnalyzerPlugin()
-  ]
-};
+  plugins: [new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ }), new BundleAnalyzerPlugin()],
+}
